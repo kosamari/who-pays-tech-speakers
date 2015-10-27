786ea204f8381038985199aa70d3f1e849df2211
kosamari <mariko.kosaka@gmail.com>
2015-10-27 10:25:07 -0400

    layout change

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ app.set('view engine', 'html');
 app.engine('html', hbs.__express);
 app.use(favicon(__dirname + '/public/img/favicon.ico'));
 hbs.registerPartial('ga', fs.readFileSync(__dirname + '/views/ga.html', 'utf8'));
+hbs.registerPartial('footer', fs.readFileSync(__dirname + '/views/footer.html', 'utf8'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.multipart());
@@ -71,7 +72,13 @@ app.get('/data.csv', function(req, res) {
 app.get('/:id', function(req, res, next) {
   db.find(req.params.id, function(err, data) {
     if(err){return notfound(req,res)};
-    res.render('submission',{_id:data[0]._id, submitted:data[0].submitted, report:data[0].report.join(' ')});
+    var text = data[0].report.join(' ');
+
+    res.render('submission',{
+      _id:data[0]._id,
+      submitted:data[0].submitted,
+      report:text,
+      tweet:'"'+text.substr(0,56)+'..." - Who pays conference speakers?'});
   });
 });
 
