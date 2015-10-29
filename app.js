46fa9cb711383f0c8d11ee949b3254da84c21b8d
kosamari <mariko.kosaka@gmail.com>
2015-10-29 00:38:08 -0400

    adding fix for ga

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var port = 5070;
 
 var fields = ['invite_type','talk_type','event_name','event_location','travel_type','ticket_type','fee','currency','travel_assistance','travel_assistance_by_employer','time_off','speaking_slot','speaking_slot_unit','prep_time','prep_time_unit','experience','gender','expertise','speaking_years','event_type','additional_info'];
 var fieldNames =['Invite Type','Talk Type','Event Name','Event Location','Travel Type','Ticket Type','Fee','Currency','Travel Assistance','Travel Assistance by Employer','Time Off','Speaking Slot','Speaking Slot Unit','Prep Time','Prep Time Unit','Experience','Gender','Expertise','Speaking Years','Event Type','Additional Info'];
+var ga = false;
 
 function submissionDate(){
   var monthNames = [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ];
@@ -29,7 +30,10 @@ app.set('view engine', 'html');
 app.engine('html', hbs.__express);
 app.use(favicon(__dirname + '/public/img/favicon.ico'));
 fs.exists(__dirname + '/views/ga.html', function (exists) {
-  if(exists){ hbs.registerPartial('ga', fs.readFileSync(__dirname + '/views/ga.html', 'utf8')); }
+  if(exists){
+    ga = true;
+    hbs.registerPartial('ga', fs.readFileSync(__dirname + '/views/ga.html', 'utf8'));
+  }
 });
 hbs.registerPartial('footer', fs.readFileSync(__dirname + '/views/footer.html', 'utf8'));
 app.use(express.json());
@@ -39,7 +43,7 @@ app.use(express.static(__dirname + '/public'));
 app.listen(port);
 
 app.get('/', function(req, res) {
-  res.render('index');
+  res.render('index',{ga:ga});
 });
 
 app.post('/submit', function(req, res) {
@@ -73,11 +77,12 @@ app.get('/data.csv', function(req, res) {
 
 app.get('/:id', function(req, res, next) {
   db.find(req.params.id, function(err, data) {
-    if(err){return notfound(req,res)};
+    if(err){return notfound(req,res);}
     var text = data[0].report.join(' ');
 
     res.render('submission',{
       _id:data[0]._id,
+      ga:ga,
       submitted:data[0].submitted,
       report:text,
       tweet:'"'+text.substr(0,56)+'..." - Who pays conference speakers?'});
