f2e9e1c1d43bd73ddfbcd4df2478f587952882fd
Alex Booker <alexbooker@fastmail.im>
2015-10-30 14:31:38 +0000

    run both query commands at the same time

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var db = require('./db').init();
 
 var report = require('./report');
 var port = 5070;
+var async = require('async');
 
 var fields = ['invite_type','talk_type','event_name','event_location','travel_type','ticket_type','fee','currency','travel_assistance','travel_assistance_by_employer','time_off','speaking_slot','speaking_slot_unit','prep_time','prep_time_unit','experience','gender','expertise','speaking_years','event_type','additional_info'];
 var fieldNames =['Invite Type','Talk Type','Event Name','Event Location','Travel Type','Ticket Type','Fee','Currency','Travel Assistance','Travel Assistance by Employer','Time Off','Speaking Slot','Speaking Slot Unit','Prep Time','Prep Time Unit','Experience','Gender','Expertise','Speaking Years','Event Type','Additional Info'];
@@ -83,12 +84,13 @@ app.get('/reports/page/:pageNum(\\d+)', function(req, res) {
 });
 
 app.get('/autoCompleteData', function(req, res) {
-  db.findAllEventNames(function(err, eventNames) {
-    db.findAllEventLocations(function(err1, eventLocations)  {
-      res.json({
-        eventNames: eventNames,
-        eventLocations: eventLocations
-      });
+  async.series([
+    db.findAllEventNames,
+    db.findAllEventLocations
+  ], function(err, results) {
+    res.json({
+      eventNames: results[0],
+      eventlocations: results[1]
     });
   });
 });
